3f0bd89fa01d483a76c72c2d51c5ed8bb73b10db
Jackbox12345 <lapitanchristian258@gmail.com>
2025-09-29 02:49:17 +0800

    additional backend for autofill

--- app/Http/Controllers/Api/PayslipUserController.php
+++ app/Http/Controllers/Api/PayslipUserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\PayslipUser;
+use App\Models\UserDepartment;
 
 class PayslipUserController extends Controller
 {
@@ -12,11 +13,22 @@ class PayslipUserController extends Controller
     {
 
 
-            $result = PayslipUser::from('users as u')
-            ->join('Departments as d', 'u.access', '=', 'd.access')
-            ->where('u.username', $request->ntlogin)
-            ->select('u.*', 'd.dept', 'd.cat')
+           $result = PayslipUser::where('username', 'like', '%' . $request->ntlogin . '%')
+            ->join('Departments as d', function($join) {
+                $join->on('users.access', '=', 'd.access')
+                    ->whereRaw('d.idDepartments = (
+                        SELECT MAX(idDepartments)
+                        FROM Departments
+                        WHERE Departments.access = users.access
+                    )');
+            })
+            ->select('users.name',
+            'users.empno as employeeId',
+            'users.username as ntlogin',
+            'd.idDepartments','d.dept as department')
             ->get();
+
+            
 
         return response()->json(['message' => $result]);
     }
